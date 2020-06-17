c85f9083910091d22ada79bd70f62bf21a6c2037
Sarah <s.schauppenlehner@gmx.at>
2020-06-17 18:37:23 +0200

    Add Quiz and Question Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
